2e30f7d410c8c6b632f4994ef7a265f7eeec688f
noordseoostende <voropayev1@mail.ru>
2022-03-26 22:32:22 +0100

    check button menu

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './styles/Header.scss'
+
+const Header = () => {
+  const [active, setActive] = useState(false)
+  const  showItem = () => {
+    setActive(!active)
+  }
+  return (
+    <div className='header'>
+      <div className="header__container">
+
+        <div className="header__text">
+          <h1>GRAD.</h1>
+        </div>
+        <nav className={active ? 'navbar active' : 'navbar'}>
+          <ul>
+            <li>
+              <Link to='/'>Verzameling</Link>
+            </li>
+            <li>
+              <Link to='/'>Merk</Link>
+            </li>
+            
+            <li>
+              <Link to='/'>Contact</Link>
+            </li>
+            <li >
+              <Link to='/' to={showItem}>Menu</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Header
